9214bad2f22ee4d70166a08df686e3c998ca017f
User <huseynovali612@gmail.com>
2023-07-27 23:56:40 +0400

    video controller complated

--- server/controller/videoController.js
+++ server/controller/videoController.js
@@ -1,6 +1,6 @@
 const Video = require("../model/videoSchema");
 const multer = require("multer");
-const { uploadFile, getFile } = require("../config/s3.js");
+const { uploadFile, getFile, deleteFile } = require("../config/s3.js");
 const User = require("../model/userSchema");
 const videoAndCoverImageUpload = multer({ dest: "upload/videos/" }).fields([
   { name: "video", maxCount: 1 },
@@ -14,8 +14,12 @@ const VideoController = {
         if (err) {
           return res.status(400).json({ message: "Video and cover image upload failed." });
         }
-
         const { title, description, userid } = req.body;
+        const user = await User.findById(userid);
+        if (!user) {
+          return res.status(404).json({ message: "User not found!" });
+        }
+
 
         const videoFile = req.files["video"][0];
         const uploadedVideo = await uploadFile(videoFile);
@@ -31,10 +35,13 @@ const VideoController = {
         const coverImageFile = req.files["coverImage"][0];
         const uploadedImage = await uploadFile(coverImageFile);
         const imagePath = uploadedImage.Location.split("/").pop();
-
         newVideo.coverImageid = imagePath;
-
         await newVideo.save();
+
+
+        user.videos.push(newVideo._id);
+        await user.save();
+
 
         res.status(201).json({ message: "Video and Cover Image added successfully!", newVideo });
       });
@@ -48,6 +55,54 @@ const VideoController = {
     const result = getFile(Key)
     result.pipe(res)
   },
+  deleteVideo : async (req, res) => {
+    const videoId = req.params.videoId;
+  
+    try {
+
+      const video = await Video.findByIdAndDelete(videoId);
+      if (!video) {
+        return res.status(404).json({ message: "Video not found!" });
+      }
+  
+      const videoFileKey = video.videoawsid;
+      const coverimgFileKey = video.coverImageid;
+      if (videoFileKey) {
+        await deleteFile(videoFileKey);
+      }
+      if (coverimgFileKey) {
+        await deleteFile(coverimgFileKey);
+      }
+      await User.findByIdAndUpdate(video.userid, { $pull: { videos: video._id } });
+      res.status(200).json({ message: "Video deleted successfully!" });
+    } catch (error) {
+      res.status(500).json({ message: "An error occurred while deleting the video.", error: error.message });
+    }
+  },
+  
+  videoEdit: async (req, res) => {
+    const videoId = req.params.videoId;
+    const { title, description } = req.body;
+
+    try {
+      const video = await Video.findById(videoId);
+      if (!video) {
+        return res.status(404).json({ message: "Video not found!" });
+      }
+
+      video.title = title || video.title; 
+      video.description = description || video.description; 
+    
+      await video.save();
+
+      res.status(200).json({ message: "Video updated successfully!", video });
+    } catch (error) {
+      res.status(500).json({ message: "An error occurred while updating the video.", error: error.message });
+    }
+  },
+
+
+
 };
 
 module.exports = VideoController;
--- server/router/videoRouter.js
+++ server/router/videoRouter.js
@@ -3,7 +3,12 @@ const VideoController = require("../controller/videoController");
 
 const videoRouter = express.Router();
 
+
+videoRouter.get("/getvideo/:key",VideoController.getVideo)
 videoRouter.post("/",VideoController.addVideo)
+videoRouter.delete("/:videoId",VideoController.deleteVideo)
+videoRouter.put("/:videoId",VideoController.videoEdit)
+
 
 
 
