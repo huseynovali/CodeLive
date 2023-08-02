9f626424032d3348f016c7ed6e360b54483fab7e
User <huseynovali612@gmail.com>
2023-08-02 18:30:03 +0400

    config sidebar menu

--- client/src/components/MenuBar/MenuBar.jsx
+++ client/src/components/MenuBar/MenuBar.jsx
@@ -1,26 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./MenuBar.css"
 import { Link } from "react-router-dom";
-import { AiOutlineHome, AiOutlineMenu, AiOutlineYoutube,AiOutlineUser } from 'react-icons/ai';
+import { AiOutlineHome, AiOutlineMenu, AiOutlineYoutube, AiOutlineUser } from 'react-icons/ai';
 import { LiaUsersSolid } from 'react-icons/lia';
 import { BiCodeAlt } from 'react-icons/bi';
 import { CiStreamOn } from 'react-icons/ci';
-import { motion, useDragControls } from 'framer-motion';
+import { motion, useMotionValue, useVelocity } from 'framer-motion';
 
 function MenuBar() {
   const [open, setOpen] = useState(false)
-  const controls = useDragControls()
-
-  function startDrag(event) {
-    controls.start(event)
-  }
 
   const toggleMenu = () => {
     setOpen(!open);
   };
+
+
+
   return (
-    <>  <div onPointerDown={startDrag} >salam</div>
-      <motion.div drag="x" dragControls={controls} />
+    <>
 
       <div className={`sidemenu__section fixed  p-3   text-2xl right-10 top-[40%] z-10 select-none ${!open ? 'rounded-ss-xl rounded-ee-xl' : "rounded-3xl"}`}>
 
@@ -52,13 +49,17 @@ function MenuBar() {
           <Link>
             <motion.li className='hover:bg-blue-700 p-3 rounded-full'><AiOutlineUser /></motion.li>
           </Link>
-          
+
         </motion.ul>
 
 
 
 
-      </div></>
+      </div>
+
+
+
+    </>
 
   )
 }
