eae24773b4033c1dd5ce73d6fa0ddfb3e95f806f
User <huseynovali612@gmail.com>
2023-08-09 21:39:06 +0400

    videos config

--- client/src/components/profileComponents/ProfileComponents/MyVideo.jsx
+++ client/src/components/profileComponents/ProfileComponents/MyVideo.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-import moment from 'moment-timezone';
+import moment from 'moment';
 import coverImg from "../../../img/video-icon-17.png"
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 function MyVideo() {
   const data = useSelector(state => state.dataSlice.user)
@@ -11,25 +11,23 @@ function MyVideo() {
   const goToVideo = (id) => {
     navigate(`/video/${id}`, { state: { from: location.pathname } });
   }
-const sortedVideos = [...data?.videos].sort((a, b) => moment(b.uploadDate) - moment(a.uploadDate));
+
+
   return (
     <div className='flex gap-5 gap-y-9 flex-wrap cursor-pointer '>
-
       {
-        
-        sortedVideos?.map(item => {
+        data?.videos?.map(item => {
           return <div className="user__videos hover:scale-[1.05] transition-all" onClick={() => goToVideo(item._id)}>
             <div className="video__cover__img">
+              {console.log(item?.coverImageid)}
               <img src={item?.coverImageid ? `http://localhost:8080/accountimg/images/${item?.coverImageid}` : coverImg} alt="video cover image" className='bg-slate-400 h-[200px] w-[300px] object-cover rounded-md' />
               <h1 className='text-white'>{item?.title}</h1>
-              {console.log(moment().tz("Asia/Baku").format())}
-              <p className='text-white'> {moment(item?.uploadDate).tz('Asia/Baku').startOf('day').fromNow()}</p>
+              {console.log(moment().format())}
+              <p className='text-white'> {moment(item?.uploadDate).startOf('day').fromNow()}</p>
             </div>
           </div>
-
         })
       }
-
     </div>
   )
 }
